191db2237a6e3e81b09a9810da24eb66bbff4141
Rohan <rohanrao360@gmail.com>
2023-09-21 15:18:57 +0530

    added comments

--- timer.js
+++ timer.js
@@ -11,21 +11,22 @@ function stopTimer() {
 }
 
 function resetTimer() {
-    clearInterval(timerInterval);
-    minutes = 0;
+    clearInterval(timerInterval); //clear the interval
+    minutes = 0; //reset the values
     seconds = 0;
     updateTimerDisplay();
 }
 
 function updateTimer() {
-    seconds++;
-    if (seconds === 60) {
+    seconds++; //for every 1000ms increment the seconds variable value
+    if (seconds === 60) { //when it is 60 reset seconds variable and start incrementing the minutes
         minutes++;
         seconds = 0;
     }
-    updateTimerDisplay();
+    updateTimerDisplay(); //update the display after value is updated
 }
 
+//function to update the display
 function updateTimerDisplay() {
     const timerMinutes = document.querySelector('.timerMinutes');
     const timerSeconds = document.querySelector('.timerSeconds');
